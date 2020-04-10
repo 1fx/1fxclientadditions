a9b2df696dc7ca32ccbe2abbc61023b782434471
AJSchat <ajschat@gmail.com>
2020-04-10 20:29:29 +0200

    Changed project license and updated license blocks of project files.

--- rocmod/cgame/cg_stats.c
+++ rocmod/cgame/cg_stats.c
@@ -1,5 +1,22 @@
-// Copyright (C) 2003 Caleb Gingles
-//
+/*
+===========================================================================
+Copyright (C) 2003, Caleb Gingles
+
+This file is part of the 1fx. Client Additions source code.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
+===========================================================================
+*/
 
 #include "cg_local.h"
 
